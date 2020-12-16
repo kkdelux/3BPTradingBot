73a94847ff1904423eccfad446e86db897cafbdc
Kyle strem <kylestrem@gmail.com>
2020-12-16 11:18:30 -0800

    Implemented skeleton for passive running of application on cloud server

---
+++ api.py
@@ -0,0 +1,79 @@
+# API.py
+# Hold lists of api classes to use with application
+
+import config as conf
+import requests
+import json
+
+class BaseAPI:
+    """Base API Class"""
+
+    def __init__(self, base_url, endpoint):
+        self._API_BASE_URL  = base_url
+        self._ENDPOINT      = endpoint
+
+    def request(self, method, headers, params):
+        url = self._API_BASE_URL + self._ENDPOINT
+
+        response = None
+        if headers == None and params == None:
+            response = requests.request(method, url)
+        elif headers == None and params != None:
+            response = requests.request(method, url, params=params)
+        elif headers != None and params == None:
+            response = requests.request(method, url, headers=headers)
+        else:
+            response = requests.request(method, url, headers=headers, params=params)
+
+        return response
+
+    def get(self, headers=None, params=None):
+        return self.request("GET", headers, params)
+
+    def request_json(self, method="GET", headers=None, params=None):
+        print(self, method, headers, params)
+
+        response = self.request(method, headers, params)
+        return json.loads(response.text)
+
+    def get_json(self, headers=None, params=None):
+        return self.request_json("GET", headers, params)
+
+class AlpacaAPI(BaseAPI):
+    """Base Alpaca API Class"""
+
+    def __init__(self, api_key_id, api_key, base_url, endpoint):
+        self._API_KEY_ID    = api_key_id
+        self._API_KEY       = api_key
+        super().__init__(base_url, endpoint)
+
+    def request(self, method, params):
+        headers = {"APCA-API-KEY-ID": self._API_KEY_ID, "APCA-API-SECRET-KEY": self._API_KEY}
+        url = self._API_BASE_URL + self._ENDPOINT
+
+        response = requests.request(method, url, headers=headers, params=params)
+
+        return response
+
+    def get(self, params=None):
+        return self.request("GET", params)
+
+    def request_json(self, method="GET", params=None):
+        response = self.request(method, params)
+        return json.loads(response.text)
+
+    def get_json(self, params=None):
+        return self.request_json("GET", params)
+
+
+class AlpacaPaperDataAPI(AlpacaAPI):
+    """Base Alpaca Paper API Class"""
+
+    def __init__(self, endpoint):
+        super().__init__(conf.ALPACA_PAPER_API_KEY_ID, conf.ALPACA_PAPER_API_SECRET_KEY, conf.ALPACA_BASE_DATA_URL, endpoint)
+
+class AlpacaPaperTradingAPI(AlpacaAPI):
+    """Base Alpaca Paper API Class"""
+
+    def __init__(self, endpoint):
+        super().__init__(conf.ALPACA_PAPER_API_KEY_ID, conf.ALPACA_PAPER_API_SECRET_KEY, conf.ALPACA_PAPER_BASE_TRADING_URL, endpoint)
---
+++ live15.py
@@ -0,0 +1,48 @@
+# Live.py
+# Main application file for running application in cloud on 15 min 3BP timeframe
+
+# imports
+import pytz
+import datetime
+
+from api import AlpacaPaperDataAPI, AlpacaPaperTradingAPI
+# setup
+
+## APIs
+
+# Alpaca
+bars_api = AlpacaPaperDataAPI("/v1/bars/")
+calendar_api = AlpacaPaperTradingAPI("/v2/calendar")
+
+eastern = pytz.timezone('America/New_York')
+today = datetime.datetime.today().astimezone(eastern)
+
+currenttime = datetime.datetime.now().astimezone(eastern)
+prevtime = datetime.datetime.now().astimezone(eastern)
+# main loop
+while (True):
+    # update current time
+    currenttime = datetime.datetime.now().astimezone(eastern)
+
+    # determine if it is a new day
+    if currenttime.day != prevtime.day:
+        # new day
+        today = datetime.datetime.today().astimezone(eastern)
+
+    # determine if the market is open today
+    calendar_dt = today.strftime("%Y-%m-%d")
+    is_open = calendar_api.get_json({"start": calendar_dt, "end": calendar_dt})[0]["date"] == calendar_dt
+
+    if is_open:
+        # if time is >= 9:30 AM New York time, collect premarket top gainers
+
+        # if time is >= 9:45 AM New York time, collect first 15 min bar from premarket top gainers
+        # determine if any of the premarket gainers have a good first bar
+
+        # if time is >= 10:00 AM New York time, collect second 15 min bar from premarket top gainers
+        # determine if any of the premarket gainers with a good first bar, have a good second bar
+
+        # if any have a good second bar, determine entry point
+
+        # place orders
+        
